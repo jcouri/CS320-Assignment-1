2637ba7ec95a6b68c26a5a73fa3277b4c09ec051
jcouri <josephmcouri@gmail.com>
2019-01-15 15:33:29 -0800

    CS320 Assignment 1

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # CS320-Assignment-1
+Hashtable, BinarySearchTree, and BalancedTree all implement the DictionaryADT interface. The PhoneBook class uses one of the three to store phone numbers and names.
---
+++ prog1_1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+printf("Assignment #1-1, Joseph Couri, josephmcouri@gmail.com\n");
+char firstname[20];
+char lastname[20];
+printf("What is your name?\n");
+scanf("%s", firstname);
+scanf("%s", lastname);
+printf("Hello %s %s!\n", firstname, lastname);
+return 0;
+}
